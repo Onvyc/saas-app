d8365767d780d9939f6911b18ec3b6d7f5f23af4
Onvyc <omar.noe.vic@gmail.com>
2026-01-12 13:01:52 -0600

    Implement Clerk Suscriptions

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,19 +1,46 @@
 import CompanionForm from '@/components/ui/CompanionForm';
+import { newCompanionPermissions } from '@/lib/actions/companion.actions';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function NewCompanion() {
   const { userId } = await auth();
-
   if (!userId) redirect('/sign-in');
+
+  const canCreateCompanion = await newCompanionPermissions();
 
   return (
     <main className="min-lg:w1/3 min-md:w2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
-        <h1>Companion Builder</h1>
+      {canCreateCompanion ? (
+        <article className="w-full gap-4 flex flex-col">
+          <h1>Companion Builder</h1>
 
-        <CompanionForm />
-      </article>
+          <CompanionForm />
+        </article>
+      ) : (
+        <article className="companion-limit">
+          <Image
+            src="/images/limit.svg"
+            alt="Companion limit"
+            width={360}
+            height={230}
+          />
+          <div className="cta-badge">Upgrade your plan</div>
+          <h1>You've Reached Your Limit</h1>
+          <p>
+            You've reached your companion limit. Upgrade to create more
+            companions and premium features
+          </p>
+          <Link
+            href="/suscription"
+            className="btn-primary w-full justify-center"
+          >
+            Upgrade My Plan
+          </Link>
+        </article>
+      )}
     </main>
   );
 }
